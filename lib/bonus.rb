545fccd99853896ed938b6406d36d6d5c3c9eb31
jose luis <cortezluisjose@gmail.com>
2019-07-03 02:34:11 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -22,7 +22,7 @@ def bonus
 
   epic_tragedy[:Montague][:patriarch][:name] = "Jose Cortez"
 
-  
+  puts epic_tragedy
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
