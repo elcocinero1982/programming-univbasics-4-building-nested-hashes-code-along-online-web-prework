c7eaf403b8177638ce051b9cf17175259b37cbe7
jose luis <cortezluisjose@gmail.com>
2019-07-03 01:59:31 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,13 +1,16 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      :patriarch => {}
+      :patriarch => {},
       :matriarch => {},
       :hero => {},
       :heroine_friends => []
       }, 
     :capulet => {
-    
+    :patriarch => {},
+    :matriarch => {},
+    :heroine =. {},
+    :heroine_friends => []
       }}
 
   
